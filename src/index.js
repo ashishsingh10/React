ff97d6607d1be290a0bfdbab17be544fc88ccf86
Ashish Singh <ashish.singh24785@gmail.com>
2023-09-09 21:38:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from "react-dom";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './App'; //dont have to give .js file full name as js file is default file
+
+ReactDom.render(
+    <App/>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
