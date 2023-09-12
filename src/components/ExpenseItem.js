761d0b1d3353a69c1e9e668c550d8bccb927b355
Ashish Singh <ashish.singh24785@gmail.com>
2023-09-12 11:26:40 +0530

    8

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){
+function ExpenseItem(props){
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     return (
         <div className="expense-item">
-            <div>September 2022</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+
+
+            </div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$100</div>
+                <h2>{ props.title }</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
